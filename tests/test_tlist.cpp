4dedc25772ca35129a687d2eb465694d545db93f
Sweetbread <sweetbread@coders-squad.com>
2024-10-14 13:40:18 +0300

    feat: tlist lib

---
+++ tests/test_tlist.cpp
@@ -0,0 +1,22 @@
+// Copyright 2024 Gleb Zaharov
+
+#include <gtest.h>
+#include "../lib_tlist/tlist.h"
+
+TEST(TList, add_front) {
+  TList<int> list;
+  list.add_front(5);
+  list.add_front(10);
+
+  ASSERT_EQ(5, list.get(1));
+  ASSERT_EQ(10, list.get(0));
+}
+
+TEST(TList, add_back) {
+  TList<int> list;
+  list.add_back(5);
+  list.add_back(10);
+
+  ASSERT_EQ(5, list.get(0));
+  ASSERT_EQ(10, list.get(1));
+}
