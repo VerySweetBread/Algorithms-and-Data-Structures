105a2406e67204de73dd421a6b17c7f69365012d
Sweetbread <sweetbread@coders-squad.com>
2024-12-23 14:57:05 +0300

    feat: Add CMonom

---
+++ lib_monom/cmonom.h
@@ -0,0 +1,63 @@
+// Copyright 2024 Gleb Zaharov
+
+#pragma once
+
+#include <stdlib.h>
+#include <iostream>
+
+typedef typeof(sizeof(0)) size_t;
+
+class CMonom {
+    size_t _coef;
+    size_t _p1, _p2, _p3;
+
+ public:
+    CMonom(size_t coef, size_t p1, size_t p2, size_t p3) {
+        _coef = coef;
+        _p1 = p1;
+        _p2 = p2;
+        _p3 = p3;
+    };
+
+    size_t get_coef() { return _coef; }
+    size_t get_p1() { return _p1; }
+    size_t get_p2() { return _p2; }
+    size_t get_p3() { return _p3; }
+
+    CMonom operator= (CMonom const& m) {
+        _coef = m._coef;
+        _p1 = m._p1;
+        _p2 = m._p2;
+        _p3 = m._p3;
+
+        return *this;
+    }
+
+    bool operator== (CMonom const& m) {
+        if (_p1 == m._p1 && _p2 == m._p2 && _p3 == m._p3)
+            return true;
+        return false;
+    };
+    bool operator!= (CMonom const& m) {
+        return !(*this == m);
+    };
+
+    CMonom operator+ (CMonom const& m) {
+        if (*this != m) throw std::logic_error("Monoms are not similar");
+        return CMonom(_coef + m._coef, _p1, _p2, _p3);
+    };
+    CMonom operator- (CMonom const& m) {
+        if (*this != m) throw std::logic_error("Monoms are not similar");
+        return CMonom(_coef - m._coef, _p1, _p2, _p3);
+    };
+    CMonom operator* (CMonom const& m) {
+        return CMonom(_coef * m._coef, _p1+m._p1, _p2+m._p2, _p3+m._p3);
+    };
+    CMonom operator/ (CMonom const& m) {
+        return CMonom(_coef / m._coef, _p1-m._p1, _p2-m._p2, _p3-m._p3);
+    };
+
+    CMonom operator-() const {
+        return CMonom(-_coef, _p1, _p2, _p3);
+    }
+};
---
+++ tests/test_monom.cpp
@@ -0,0 +1,131 @@
+// Copyright 2024 Gleb Zaharov
+
+#include <gtest.h>
+#include "../lib_monom/cmonom.h"
+
+TEST(CMonom, plus) {
+  // x + x = 2x
+  CMonom a(1, 1, 0, 0);
+  CMonom b(1, 1, 0, 0);
+  CMonom c = a + b;
+
+  ASSERT_EQ(c.get_coef(), 2);
+  ASSERT_EQ(c.get_p1(), 1);
+  ASSERT_EQ(c.get_p2(), 0);
+  ASSERT_EQ(c.get_p3(), 0);
+
+
+  // y^2z + 2y^2z = 3y^2z
+  a = CMonom(1, 0, 2, 1);
+  b = CMonom(2, 0, 2, 1);
+  c = a + b;
+
+  ASSERT_EQ(c.get_coef(), 3);
+  ASSERT_EQ(c.get_p1(), 0);
+  ASSERT_EQ(c.get_p2(), 2);
+  ASSERT_EQ(c.get_p3(), 1);
+}
+
+
+TEST(CMonom, minus) {
+  // 2x - x = x
+  CMonom a(2, 1, 0, 0);
+  CMonom b(1, 1, 0, 0);
+  CMonom c = a - b;
+
+  ASSERT_EQ(c.get_coef(), 1);
+  ASSERT_EQ(c.get_p1(), 1);
+  ASSERT_EQ(c.get_p2(), 0);
+  ASSERT_EQ(c.get_p3(), 0);
+
+
+  // 3y^2z = y^2z = 2y^2z
+  a = CMonom(3, 0, 2, 1);
+  b = CMonom(1, 0, 2, 1);
+  c = a - b;
+
+  ASSERT_EQ(c.get_coef(), 2);
+  ASSERT_EQ(c.get_p1(), 0);
+  ASSERT_EQ(c.get_p2(), 2);
+  ASSERT_EQ(c.get_p3(), 1);
+}
+
+TEST(CMonom, plus_throw) {
+  // x + xz = ?
+  CMonom a(1, 1, 0, 0);
+  CMonom b(1, 1, 0, 1);
+  
+  EXPECT_THROW(a + b, std::logic_error);
+
+  // y^2z + 2xyz = ?
+  a = CMonom(1, 0, 2, 1);
+  b = CMonom(2, 1, 1, 1);
+  EXPECT_THROW(a + b, std::logic_error);
+}
+
+TEST(CMonom, minus_throw) {
+  // x - xz = ?
+  CMonom a(1, 1, 0, 0);
+  CMonom b(1, 1, 0, 1);
+  
+  EXPECT_THROW(a - b, std::logic_error);
+
+  // y^2z - 2xyz = ?
+  a = CMonom(1, 0, 2, 1);
+  b = CMonom(2, 1, 1, 1);
+  EXPECT_THROW(a - b, std::logic_error);
+}
+
+TEST(CMonom, mul) {
+  // 2x * yz = 2xyz
+  CMonom a = CMonom(2, 1, 0, 0);
+  CMonom b = CMonom(1, 0, 1, 1);
+  CMonom c = a * b;
+
+  ASSERT_EQ(c.get_coef(), 2);
+  ASSERT_EQ(c.get_p1(), 1);
+  ASSERT_EQ(c.get_p2(), 1);
+  ASSERT_EQ(c.get_p3(), 1);
+
+  // 2x * 3x^2yz^5 = 6x^3yz^5
+  a = CMonom(2, 1, 0, 0);
+  b = CMonom(3, 2, 1, 5);
+  c = a * b;
+
+  ASSERT_EQ(c.get_coef(), 6);
+  ASSERT_EQ(c.get_p1(), 3);
+  ASSERT_EQ(c.get_p2(), 1);
+  ASSERT_EQ(c.get_p3(), 5);
+}
+
+TEST(CMonom, div) {
+  // 2xyz / yz = 2x
+  CMonom a = CMonom(2, 1, 1, 1);
+  CMonom b = CMonom(1, 0, 1, 1);
+  CMonom c = a / b;
+
+  ASSERT_EQ(c.get_coef(), 2);
+  ASSERT_EQ(c.get_p1(), 1);
+  ASSERT_EQ(c.get_p2(), 0);
+  ASSERT_EQ(c.get_p3(), 0);
+
+  // 6x^3yz^5 / 3x^2yz^5 = 2x
+  a = CMonom(6, 3, 1, 5);
+  b = CMonom(3, 2, 1, 5);
+  c = a / b;
+
+  ASSERT_EQ(c.get_coef(), 2);
+  ASSERT_EQ(c.get_p1(), 1);
+  ASSERT_EQ(c.get_p2(), 0);
+  ASSERT_EQ(c.get_p3(), 0);
+}
+
+TEST(CMonom, unaryminus) {
+  CMonom a = CMonom(2, 3, 5, 1);
+  CMonom b = -a;
+
+  ASSERT_EQ(b.get_coef(), -2);
+  ASSERT_EQ(b.get_p1(), 3);
+  ASSERT_EQ(b.get_p2(), 5);
+  ASSERT_EQ(b.get_p3(), 1);
+}
